98173993275a711c0f8d7afb40823e863cb4998e
Ankit <ankitmaurya9560@gmail.com>
2024-05-28 16:58:24 +0530

    file added system done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -9,7 +9,16 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    fs.readdir(`./files`, (err, files) => {
+        console.log(files);
+        res.render("index", { files: files });
+    });
+});
+
+app.post("/create", (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+        res.redirect("/");
+    });
 });
 
 app.listen(3000);
