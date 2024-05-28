c2ada83642ffb0f3d689a64c40bb683c814d6642
Ankit <ankitmaurya9560@gmail.com>
2024-05-28 15:25:19 +0530

    Practice project

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
+
 const express = require("express");
 const app = express();
 const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index");
-});
-
-app.get("/profile/:username", (req, res,) => {
-    res.send(`welcome ${req.params.username}`);
 });
 
 app.listen(3000);
