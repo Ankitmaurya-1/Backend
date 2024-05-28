7e20102badb8b5e61a25368c3527bb49f2b492e2
Ankit <ankitmaurya9560@gmail.com>
2024-05-28 07:07:32 +0530

    Ejs Pages

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("It's Working");
+    res.render("index");
 });
 
 app.listen(3000);
