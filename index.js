4a0b6253b6b37fec83e476b3faf1115845924841
Ankit <ankitmaurya9560@gmail.com>
2024-05-29 19:18:51 +0530

    Read file done

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     fs.readdir(`./files`, (err, files) => {
-        console.log(files);
+        if (err) {
+            console.log("Error reading files:", err);
+            return res.status(500).send("Error reading files");
+        }
         res.render("index", { files: files });
     });
 });
 
+app.get("/file/:filename", (req, res) => {
+    const filepath = path.join(__dirname, "files", req.params.filename);
+    console.log("Reading file:", filepath);  // Log the file path
+    fs.readFile(filepath, "utf-8", (err, filedata) => {
+        if (err) {
+            console.log("Error reading file:", err);
+            return res.status(500).send("Error reading file");
+        }
+        console.log("File data:", filedata);  // Log the file data
+        res.render("show", { filename: req.params.filename, filedata: filedata });
+    });
+});
+
 app.post("/create", (req, res) => {
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+    const filename = `${req.body.title.split(' ').join('')}.txt`;
+    const filepath = path.join(__dirname, "files", filename);
+    fs.writeFile(filepath, req.body.details, (err) => {
+        if (err) {
+            console.log("Error writing file:", err);
+            return res.status(500).send("Error writing file");
+        }
         res.redirect("/");
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
