c275b5b3b98d9b6377cca228cf4582fdad7f03cd
Ankit <ankitmaurya9560@gmail.com>
2024-06-06 19:32:36 +0530

    chal nikal

--- Auths,Cookie,session/app.js
+++ Auths,Cookie,session/app.js
@@ -1,17 +1,10 @@
-
 const express = require('express');
 const app = express();
 
-
 app.get('/', (req, res) => {
-
-    res.send("Hello World it's done");
-});
-app.get('/read', (req, res) => {
-
-    res.send("Hello World it's a read page");
+    res.send("Hello, world!");
 });
 
 app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+    console.log("Server listening on port || 3000");
 });
