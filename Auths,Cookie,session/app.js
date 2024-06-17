a0ea4e3672938dc8aa2c00701c9b3c78f710d2f4
Ankit <ankitmaurya9560@gmail.com>
2024-06-17 07:49:37 +0530

    revised jwt, cookie, sesseion

--- Auths,Cookie,session/app.js
+++ Auths,Cookie,session/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
-app.get('/', (req, res) => {
-    res.send("Hello, world!");
+
+app.use(cookieParser());
+
+
+
+
+app.get("/", (req, res) => {
+
+    let token = jwt.sign({ email: "ankit@gmail.com" }, "secret");
+    res.cookie("token", token);
+
+    res.send("cookie is set to ");
 });
 
+app.get("/read", (req, res) => {
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
+    res.send("Trying to read jwt");
+});
+
+
 app.listen(3000, () => {
-    console.log("Server listening on port || 3000");
+    console.log("Server is running on port 3000");
 });
